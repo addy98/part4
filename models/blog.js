bcd54ba6bcf60852c07c36254a475ad88b0a71aa
Addison <addisonwest98@gmail.com>
2024-04-05 12:15:52 -0400

    refactored into modules

---
+++ models/blog.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log(`error connecting to MongoDB: ${error.message}`)
+    })
+
+// transform returned documents with configurable options in setter of schema
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
