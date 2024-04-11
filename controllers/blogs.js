5a71e78ee05389391e68b9007ae1ec771eafdcd6
Addison <addisonwest98@gmail.com>
2024-04-11 17:38:51 -0400

    testing backend section done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,14 +6,33 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+    if (request.body.title == undefined || request.body.url == undefined) {
+        response.status(400).end()
+    } else {
+        const blog = new Blog(request.body)
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    }
+})
 
-    blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+    const { title, author, url, likes } = request.body
+  
+    Blog.findByIdAndUpdate(
+      request.params.id,
+      { title, author, url, likes },
+      { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedBlog => {
+      response.json(updatedBlog)
     })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
